aef2d8433d1d4f31dab6a727cee81271abe70254
Parth Pasrija <pasrijaparth77@gmail.com>
2026-06-09 18:11:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.98%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int sum = 0;
+        while (x != 0) {
+            int rem = x % 10;
+            sum = sum * 10 + rem;
+            x /= 10;
+        }
+        if (sum > INT_MAX || sum < INT_MIN) {
+            return 0;
+        }
+
+        return sum;
+    }
+};
