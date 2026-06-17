49e9a442c33eaf5d4a084f0673fbd37b86fb9334
Parth Pasrija <pasrijaparth77@gmail.com>
2026-06-17 13:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (42.57%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
-     double myPow(double x, int n) {
-        double ans;
-         ans = pow(x,n);
-         return ans;
-     }
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        if (x == -1) {
+            if (n % 2 == 0) return 1;
+            else return -1;
+        }
+        long long binForm = n;
+        if (n < 0) {
+            x = 1 / x;
+            binForm = -binForm;
+        }
+        double ans = 1;
+        while (binForm > 0) {
+            if (binForm % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            binForm /= 2;
+        }
+        return ans;
+    }
 };
