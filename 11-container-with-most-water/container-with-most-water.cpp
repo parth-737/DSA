0cc7fe4b37c9d3967aef0c67f2e8367715b581e8
Parth Pasrija <pasrijaparth77@gmail.com>
2026-06-18 14:38:15 +0530

    Time: 4 ms (20.68%) | Memory: 63 MB (10.65%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,30 +1,51 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         // int maxWater = 0 , width , area , maxHeight;
+//         // for ( int i = 0 ; i < height.size()-1 ; i++){
+//         //     for ( int j = i + 1 ; j < height.size() ; j++){
+//         //         width = j-i;
+//         //         maxHeight = min ( height[i] , height[j]);
+//         //         area = width * maxHeight;
+//         //         maxWater = max( area , maxWater);
+//         //     }
+//         // }
+//         // return maxWater;
+//         int maxWater = 0 , right = height.size() - 1 , left = 0 , width , maxHeight , area ;
+//         while (right > left) {
+//             width = right - left;
+//             maxHeight = min ( height[right] , height[left]);
+//             area = width * maxHeight ;
+//             maxWater = max ( area , maxWater );
+
+//             if (height[left] < height[right]) {
+//                 left++;
+//             } else {
+//                 right--;
+//             }
+            
+//         }
+//         return maxWater; 
+//     }
+// };
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // int maxWater = 0 , width , area , maxHeight;
-        // for ( int i = 0 ; i < height.size()-1 ; i++){
-        //     for ( int j = i + 1 ; j < height.size() ; j++){
-        //         width = j-i;
-        //         maxHeight = min ( height[i] , height[j]);
-        //         area = width * maxHeight;
-        //         maxWater = max( area , maxWater);
-        //     }
-        // }
-        // return maxWater;
-        int maxWater = 0 , right = height.size() - 1 , left = 0 , width , maxHeight , area ;
-        while (right > left) {
-            width = right - left;
-            maxHeight = min ( height[right] , height[left]);
-            area = width * maxHeight ;
-            maxWater = max ( area , maxWater );
+        int left=0, right=height.size()-1;
+        int result=INT_MIN;
 
-            if (height[left] < height[right]) {
+        while(left<right){
+            int area=min(height[left], height[right])*(right-left);
+            result=max(result, area);
+            if(height[left]<height[right]){
                 left++;
-            } else {
+            }else{
                 right--;
             }
-            
         }
-        return maxWater; 
+
+        return result;
+
     }
 };
